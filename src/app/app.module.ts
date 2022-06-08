2180298b55d262f5f6e501e8454c559cd035aa6b
Nicolas Romero <nstiromero@poligran.edu.co>
2022-06-08 17:16:23 -0500

    modificacion del module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddPeopleComponent } from './components/add-people/add-people.component';
-import { ListPeopleComponent } from './components/list-people/list-people.component';
+import { AddVehiculoComponent } from './components/add-vehiculo/add-vehiculo.component';
+import { ListVehiculoComponent } from './components/list-vehiculo/list-vehiculo.component';
 
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,8 +14,8 @@ import { ListFilterPipe } from './services/listerfilter.service';
 @NgModule({
   declarations: [
     AppComponent,
-    AddPeopleComponent,
-    ListPeopleComponent, 
+    AddVehiculoComponent,
+    ListVehiculoComponent, 
     ListFilterPipe
   ],
   imports: [
